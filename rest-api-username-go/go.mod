7559c2b8514d4660f19daf382bcda69e48e01173
Rama Rahardi <rama.orion@gmail.com>
2023-01-05 16:12:40 +0700

    Connect DB and get functions

--- rest-api-username-go/go.mod
+++ rest-api-username-go/go.mod
@@ -2,11 +2,13 @@ module rest-api-username-go
 
 go 1.18
 
-require go.mongodb.org/mongo-driver v1.11.1
+require (
+	github.com/gin-gonic/gin v1.8.2
+	go.mongodb.org/mongo-driver v1.11.1
+)
 
 require (
 	github.com/gin-contrib/sse v0.1.0 // indirect
-	github.com/gin-gonic/gin v1.8.2 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator/v10 v10.11.1 // indirect
--- rest-api-username-go/main.go
+++ rest-api-username-go/main.go
@@ -2,48 +2,115 @@ package main
 
 import (
 	"context"
-	"fmt"
+	//"fmt"
 	"log"
 
-	//"go.mongodb.org/mongo-driver/bson"
+	//"encoding/json"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+	
+	//"net/http"
+	"github.com/gin-gonic/gin"
 )
 
-type Trainer struct {
-	Name string
-	Age int
-	City string
+const host = "mongodb://localhost:27017"
+const db_name = "cil-rest-api"
+const collection_name = "cil-users"
+
+type User struct {
+	id			string	`json:"_id" bson:"_id"`
+	username	string	`json:"username" bson:"username"`
+	password	string	`json:"password" bson:"password"`
+}
+
+func GetMongoDBConnection() (*mongo.Client, error) {
+	//fmt.Println("GetMongoDBConnection")
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(host))
+	if err != nil {
+		return nil, err
+	}
+
+	err = client.Ping(context.TODO(), readpref.Primary())
+	if err != nil {
+		return nil, err
+	}
+
+	return client, nil
+}
+
+func GetMongoDBCollection (dbName string, collectionName string) (*mongo.Collection, error) {
+	//fmt.Println("GetMongoDBCollection")
+	client, err := GetMongoDBConnection()
+	if err != nil {
+		return nil, err
+	}
+
+	collection := client.Database(dbName).Collection(collectionName)
+	return collection, nil
+}
+
+func GetAllUsers(c *gin.Context) {
+    collection, err := GetMongoDBCollection(db_name, collection_name)
+    if err != nil {
+        log.Fatal(err)
+        c.String(500, err.Error())
+        return
+    }
+
+    var filter bson.M = bson.M{}
+
+    var results []bson.M
+    cur, err := collection.Find(context.TODO(), filter)
+    defer cur.Close(context.TODO())
+    if err != nil {
+        log.Fatal(err)
+        c.String(500, err.Error())
+        return
+    }
+
+    cur.All(context.TODO(), &results)
+    //json, _ := json.Marshal(results)
+    c.JSON(200, results)
+}
+
+func GetUser(c *gin.Context) {
+	//fmt.Println("GetUser")
+	collection, err := GetMongoDBCollection(db_name, collection_name)
+	if err != nil {
+		log.Fatal(err)
+		c.String(500, err.Error())
+		return
+	}
+
+	var filter bson.M = bson.M{}
+	if c.Param("id") != "" {
+		id := c.Param("id")
+		objID, _ := primitive.ObjectIDFromHex(id)
+		filter = bson.M{"_id": objID}
+	}
+
+	var results []bson.M
+	cur, err := collection.Find(context.TODO(), filter)
+	defer cur.Close(context.TODO())
+	if err != nil {
+		log.Fatal(err)
+		c.String(500, err.Error())
+		return
+	}
+
+	cur.All(context.TODO(), &results)
+	//json, _ := json.Marshal(results)
+	c.JSON(200, results)
 }
 
 func main() {
-	// Set client options
-	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
-
-	// Connect to MongoDB
-	client, err := mongo.Connect(context.TODO(), clientOptions)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Check the connection
-	err = client.Ping(context.TODO(), nil)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("Connected to MongoDB!")
-
-	// collection := client.Database("test").Collection("trainers")
-
-	// Disconnect from MongoDB
-	err = client.Disconnect(context.TODO())
-
-	if (err != nil) {
-		log.Fatal(err)
-	}
-
-	fmt.Println("Connection to MongoDB closed.")
+	router := gin.Default()
+	router.GET("/user/", GetAllUsers)
+	router.GET("/user/:id", GetUser)
+	
+	router.Run("localhost:3001")
 }
